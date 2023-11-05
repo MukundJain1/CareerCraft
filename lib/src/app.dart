eb6e19b04083d2f957b2f58a76810cf642d57c5f
ay-india <ashishyd240@gmail.com>
2023-11-05 23:15:16 +0530

    structure_updated

---
+++ lib/src/app.dart
@@ -0,0 +1,32 @@
+
+import 'package:careercraft/src/screen/pages/user_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'CareerCraft',
+          theme: ThemeData(
+            // primaryColor: primary,
+            
+            // primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      
+      child: UserFormPage(),
+    );
+  }
+}
