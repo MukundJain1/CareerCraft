b4c6b93ee169553651fd4d78bf6d40a093f92563
Mukund <mukundjain2803@gmail.com>
2023-11-08 10:08:03 +0530

    changes in hieght of appBar

--- lib/src/screen/pages/user_info/components/components.dart
+++ lib/src/screen/pages/user_info/components/components.dart
@@ -1,11 +1,11 @@
-import 'package:careercraft/constant/constant.dart';
+// import 'package:careercraft/constant/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 
 PreferredSizeWidget? get appBar => AppBar(
-      toolbarHeight: 45.h,
+      toolbarHeight: 65.h,
       // shadowColor: Color(0x00DFCCFB)   ,
       // elevation: 2,
       // leadingWidth: 60.w,
@@ -13,12 +13,11 @@ PreferredSizeWidget? get appBar => AppBar(
       automaticallyImplyLeading: true,
       title: Row(
         children: [
-
           SizedBox(width: 5.w),
           Text(
             "CareerCraft",
             style: TextStyle(
-                color: Colors.black, fontSize: 18.sp, fontWeight: FontWeight.w500),
+                color: Colors.black, fontSize: 28.sp, fontWeight: FontWeight.w500,),
           ),
           SizedBox(width: 5.w),
           // Icon(Icons.arrow_drop_down_outlined),
